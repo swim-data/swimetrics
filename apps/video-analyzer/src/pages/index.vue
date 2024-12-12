cc43d41bdbff4d930c7dc0f77e40c5efd2d90162
maurerflo <69213489+maurerflo@users.noreply.github.com>
2024-12-12 13:18:09 +0000

    feat: add VideoEditor component

--- apps/video-analyzer/src/pages/index.vue
+++ apps/video-analyzer/src/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="flex w-full h-full">
-    <CanvasArea class="basis-9/12	" />
+    <!--TODO Stack Canvas over the VideoPlayer-->
+    <VideoEditor class="basis-9/12 z-0 relative" />
+    <CanvasArea class="basis-9/12 bg-red-300 z-10 absolute top-0 left-0" />
     <Sidebar class="basis-3/12" />
   </div>
 </template>
 <script setup lang="ts">
 import CanvasArea from '../components/editor/CanvasArea.vue';
+import VideoEditor from '../components/VideoEditor.vue';
 import Sidebar from '../components/editor/Sidebar.vue';
 </script>
