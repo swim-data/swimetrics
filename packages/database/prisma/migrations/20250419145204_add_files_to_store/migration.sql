a7fe290c54d147ec6f6cba7e4642634fc8ae3aad
Niklas Kappler <mail@niklas-kappler.de>
2025-04-19 17:01:40 +0200

    feat(lib/database): add file schema
    SWIME-5

---
+++ packages/database/prisma/migrations/20250419145204_add_files_to_store/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "originalName" TEXT NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "bucket" TEXT NOT NULL,
+    "bucketPath" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
